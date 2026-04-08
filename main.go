1d1f0a417ed5a9660dad039b84bcd518f9ace237
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2026-04-07 22:34:23 -0600

    feat: add login, register and users commands

--- main.go
+++ main.go
@@ -1,27 +1,82 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jasonsoprovich/rss-aggregator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("login requires a username argument")
+	}
+
+	username := cmd.args[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return fmt.Errorf("error setting user: %w", err)
+	}
+
+	fmt.Printf("User set to: %s\n", username)
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Println("Config before:", cfg)
 
-	err = cfg.SetUser("jason")
-	if err != nil {
-		log.Fatalf("error setting user: %v", err)
+	s := &state{cfg: &cfg}
+
+	cmds := commands{
+		handlers: make(map[string]func(*state, command) error),
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config again: %v", err)
+	cmds.register("login", handlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "error: not enough arguments, a command is required")
+		os.Exit(1)
 	}
-	fmt.Println("Config after:", cfg)
+
+	cmd := command{
+		name: args[1],
+		args: args[2:],
+	}
+
+	err = cmds.run(s, cmd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
